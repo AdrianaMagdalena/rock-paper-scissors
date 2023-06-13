eec0aa8296ea9cc711c61a56fbebf9f6f5cf5670
AdrianaMagdalena <128493793+AdrianaMagdalena@users.noreply.github.com>
2023-06-13 22:44:40 +0200

    Add explanation on page about missing UI and how to play

--- script.js
+++ script.js
@@ -1,71 +1,78 @@
 console.log("Hello there. Shall we play?");
 console.log("There are 5 rounds. One that gets more points, WINS!");
+console.log("Click the button of the page if you're ready");
 
-for (let round = 1, playerPoints = 0, computerPoints = 0; round <= 5; ) {
-  console.log(`ROUND ${round}`);
+const button = document.getElementById("startGameButton");
 
-  const gameChoices = ["paper", "rock", "scissors"];
-  let computerSelection =
-    gameChoices[Math.floor(Math.random() * gameChoices.length)];
+function game() {
+  for (let round = 1, playerPoints = 0, computerPoints = 0; round <= 5; ) {
+    console.log(`ROUND ${round}`);
 
-  let playerProvidedChoice = prompt(
-    "What's your move? Paper, rock or scissors?"
-  );
-  let playerSelection = playerProvidedChoice.toLowerCase();
+    const gameChoices = ["paper", "rock", "scissors"];
+    let computerSelection =
+      gameChoices[Math.floor(Math.random() * gameChoices.length)];
 
-  let getPlayerChoice = function () {
-    return `Player's choice is: ${playerSelection}`;
-  };
-
-  let getComputerChoice = function () {
-    return `Computer's choice is: ${computerSelection}`;
-  };
-
-  console.log(getPlayerChoice());
-  console.log(getComputerChoice());
-
-  if (
-    playerSelection !== "paper" &&
-    playerSelection !== "rock" &&
-    playerSelection !== "scissors"
-  ) {
-    console.log(
-      `${playerSelection} is not a valid choice. Choose again and make sure to check your spelling.`
+    let playerProvidedChoice = prompt(
+      "What's your move? Paper, rock or scissors?"
     );
-  } else if (playerSelection === computerSelection) {
-    console.log("It's a draw! Nobody gets a point - play again!");
-    console.log(
-      `Result: Player's points: ${playerPoints} \| Computer's points: ${computerPoints}`
-    );
-  } else if (
-    (computerSelection === "paper" && playerSelection === "scissors") ||
-    (computerSelection === "scissors" && playerSelection === "rock") ||
-    (computerSelection === "rock" && playerSelection === "paper")
-  ) {
-    ++playerPoints;
-    console.log(
-      `${playerSelection} beats ${computerSelection}! Player gets a point!!!`
-    );
-    console.log(
-      `Result: Player's points: ${playerPoints} \| Computer's points: ${computerPoints}`
-    );
-    round++;
-  } else {
-    ++computerPoints;
-    console.log(
-      `${computerSelection} beats ${playerSelection}! Computer get a point!!!`
-    );
-    console.log(
-      `Result: Player's points: ${playerPoints} \| Computer's points: ${computerPoints}`
-    );
-    round++;
-  }
+    let playerSelection = playerProvidedChoice.toLowerCase();
 
-  if (playerPoints + computerPoints === 5) {
-    if (playerPoints > computerPoints) {
-      console.log("You win! Congratulations, you beat the mighty computer!");
+    let getPlayerChoice = function () {
+      return `Player's choice is: ${playerSelection}`;
+    };
+
+    let getComputerChoice = function () {
+      return `Computer's choice is: ${computerSelection}`;
+    };
+
+    console.log(getPlayerChoice());
+    console.log(getComputerChoice());
+
+    if (
+      playerSelection !== "paper" &&
+      playerSelection !== "rock" &&
+      playerSelection !== "scissors"
+    ) {
+      console.log(
+        `${playerSelection} is not a valid choice. Choose again and make sure to check your spelling.`
+      );
+    } else if (playerSelection === computerSelection) {
+      console.log("It's a draw! Nobody gets a point - play again!");
+      console.log(
+        `Result: Player's points: ${playerPoints} \| Computer's points: ${computerPoints}`
+      );
+    } else if (
+      (computerSelection === "paper" && playerSelection === "scissors") ||
+      (computerSelection === "scissors" && playerSelection === "rock") ||
+      (computerSelection === "rock" && playerSelection === "paper")
+    ) {
+      ++playerPoints;
+      console.log(
+        `${playerSelection} beats ${computerSelection}! Player gets a point!!!`
+      );
+      console.log(
+        `Result: Player's points: ${playerPoints} \| Computer's points: ${computerPoints}`
+      );
+      round++;
     } else {
-      console.log("Computer wins! Better luck next time!");
+      ++computerPoints;
+      console.log(
+        `${computerSelection} beats ${playerSelection}! Computer gets a point!!!`
+      );
+      console.log(
+        `Result: Player's points: ${playerPoints} \| Computer's points: ${computerPoints}`
+      );
+      round++;
+    }
+
+    if (playerPoints + computerPoints === 5) {
+      if (playerPoints > computerPoints) {
+        console.log("You win! Congratulations, you beat the mighty computer!");
+      } else {
+        console.log("Computer wins! Better luck next time!");
+      }
     }
   }
 }
+
+button.addEventListener("click", game);
